4086a066620de58f00b8ddd5ff6d884a953479b8
Elton Oliveira <eltonknoxville@hotmail.com>
2013-09-12 11:50:04 -0400

    Footer
    Inserção da mensagem no footer do site

--- application/views/templates/view_home.php
+++ application/views/templates/view_home.php
@@ -44,8 +44,8 @@
 	</div>
 	<div class="row">
 		<div class="span12">
-			<p>
-				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+			<p style="color-footer">
+					<strong><center>Todos os direitos reservados para Senac Amazonas</center></strong>
 			</p>
 		</div>
 	</div>
