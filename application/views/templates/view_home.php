ac829e5b2ffc17d476c52dd621a8611a65a752db
Elton Oliveira <eltonknoxville@hotmail.com>
2013-09-13 16:33:42 -0400

    Otimização da home
    foi dividido o código em outros arquivos

--- application/views/templates/view_home.php
+++ application/views/templates/view_home.php
@@ -1,42 +1,4 @@
-<div class="container">
-	<div class="row">
-		<div class="span12">
-			<img src="<?php echo base_url(); ?>assets/images/topo.png" />
-		</div>
-	</div>	
-		<div class="row">		
-			<div class="span12">
-				<div class="navbar">
-					<div class="navbar-inner new-color-nav">
-						<div class="container-fluid">
-							
-							<div class="nav-collapse collapse navbar-responsive-collapse">
-								<ul class="nav">
-									<li class="active">
-										<a href="#"><i class="icon-home icon-white"></i> &nbsp; Home</a>
-									</li>
-									<li>
-										<a href="#"><i class="icon-file icon-white"></i> &nbsp;Nova Solicitação</a>
-									</li>
-									<li>
-										<a href="#"><i class="icon-search icon-white"></i> &nbsp;Minhas Solicitações</a>
-									</li>
-									<li>
-										<a href="#modal-container-95857" data-toggle="modal"><i class="icon-thumbs-up icon-white"></i> &nbsp;Dúvidas Frequentes</a>
-									</li>
-								
-								</ul>
-								
-									</li>
-								</ul>
-							</div>
-							
-						</div>
-					</div>
-					
-				</div>
-			</div>
-		</div>
+
 		
 	<div class="row">
 		<div class="span12">
@@ -48,7 +10,7 @@
 
 				</center>
 				
-			<?php echo br(16); ?>
+			<?php echo br(15); ?>
 		</div>
 	</div>
 	<!-- Window Modal -->
@@ -70,7 +32,7 @@
 							</p>
 						</div>
 						<div class="modal-footer">
-							 <button class="btn" data-dismiss="modal" aria-hidden="true">Fechar</button>
+							 <button class="btn btn-warning" data-dismiss="modal" aria-hidden="true">Fechar</button>
 						</div>
 					</div>
 				</div>
@@ -78,11 +40,4 @@
 		</div>
 
 	<!-- End Window Modal -->
-	<div class="row">
-		<div class="span12">
-			<p style="color-footer">
-					<strong><center>Senac Amazonas - Todos os direitos reservados</center></strong>
-			</p>
-		</div>
-	</div>
-</div>
+	
