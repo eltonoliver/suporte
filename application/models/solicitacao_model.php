b215814730f407d7d970bb9d88d9d8b1ba4b1b38
Elton Oliveira <eltonknoxville@hotmail.com>
2013-09-17 14:24:02 -0400

    Criação do model solicitação
    criação do model solicitação

---
+++ application/models/solicitacao_model.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Solicitacao_model extends grocery_CRUD_Model{
+
+
+	
+
+
+
+
+
+
+}
+?>
