37b831b37e2e6adeb74103f8a485604a617db37f
Elton Oliveira <eltonknoxville@hotmail.com>
2013-09-17 14:24:35 -0400

    criação de model exemplo
    criação de model exemplo

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -4,7 +4,26 @@ class User_model extends grocery_CRUD_Model {
 
 	public function getUser(){
 
-		return $this->db->get('user')->result();
+		return $this->db->query('				
+				SELECT
+				situacao.nome,
+				usuarios.nome as user,
+				prioridade.nome,
+				solicitacao_equi.patrimonio,
+				solicitacao_equi.descricao_equi,
+				solicitacao_equi.descricao_servico,
+				solicitacao_equi.anexo,
+				solicitacao_equi.local_servico,
+				solicitacao_equi.usuario_id,
+				solicitacao_equi.data_solicitacao,
+				solicitacao_equi.data_atualizacao,
+				solicitacao_equi.data_finalizacao
+				FROM
+				solicitacao_equi
+				INNER JOIN prioridade ON solicitacao_equi.prioridade_id = prioridade.id
+				INNER JOIN situacao ON solicitacao_equi.situacao_id = situacao.id
+				INNER JOIN usuarios ON solicitacao_equi.suporte_id = usuarios.id
+			')->result();
 	}
 
 
