6d195f050c69280c0f4cc3bd9301ab784dddd141
Elton Oliveira <eltonknoxville@hotmail.com>
2013-09-18 17:14:42 -0400

    Alteração do css para o menu drop
    Alteração do css para o menu drop

--- application/views/home.php
+++ application/views/home.php
@@ -7,18 +7,18 @@
    <meta name="keywords" content="">
    <meta name="author" content="">
 
-   <title>Solicitação de Serviço</title>
- <link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+   <title>Solicitação de Serviços</title>
+
    <!--link rel="stylesheet/less" href="<?php echo base_url(); ?>assets/less/bootstrap.less" type="text/css" /-->
    <!--link rel="stylesheet/less" href="<?php echo base_url(); ?>assets/less/responsive.less" type="text/css" /-->
    <!--script src="<?php echo base_url(); ?>assets/js/less-1.3.3.min.js"></script-->
    <!--append ‘#!watch’ to the browser URL, then refresh the page. -->
    
    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
-  
+   <link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.min.css" rel="stylesheet">
    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
    <link href="<?php echo base_url(); ?>assets/css/custom.css" rel="stylesheet">
-   
+
    <?php foreach($css_files as $file): ?>
    <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
    <?php endforeach; ?>    
@@ -48,6 +48,7 @@
       {
         text-decoration: underline;
       }
+
   </style>
 
 
@@ -156,6 +157,5 @@
 
  <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script> 
  <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
-
 
 </html>
