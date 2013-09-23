0a78d10af4c7ba46fba685f073b936e059fa7e58
Elton <eltonknoxville@hotmail.com>
2013-09-22 23:07:08 -0400

    Finalização do methodo solicitacaoSistemas
    Finalizacao do methodo sistemas

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -39,9 +39,9 @@ class Home extends CI_Controller{
 			/*Deixa o campo data_solicitacao invisivel*/	 
 			$crud->field_type('data_solicitacao','invisible');
 			$crud->required_fields('descricao_equi','descricao_servico','patrimonio');
-			$crud->field_type('data_solicitacao', 'date');
 			
-			$crud->set_subject('Solicitação');
+			
+			$crud->set_subject('Solicitação - Equipamentos');
 			
 			$crud->unset_back_to_list();
 			/*Insere a data de solicitação automaticamente via callback*/
@@ -57,8 +57,38 @@ class Home extends CI_Controller{
 		
 	}
 
-	public function solcicitacaoSistema(){
+	public function solcicitacaoSistema($id = null){
+		try{
+			
+			$crud = new grocery_CRUD();		
 
+			$crud->set_theme('flexigrid');
+			$crud->set_table('solicitacao');	
+			/*set_relation('capodatabela','tabela_relacionada','chave estrangeira')*/
+			$crud->set_relation('local_servico','db_base.unidade_uni','uni_nomecompleto');
+			$crud->set_relation('sistemas_id','sistemas','nome');
+			$crud->fields('sistemas_id','anexo','descricao_servico','local_servico','data_solicitacao');
+			$crud->display_as('sistemas_id','Sistema')
+				 ->display_as('anexo','Anexo')
+				 ->display_as('descricao_servico','Descrição do Serviço')
+				 ->display_as('local_servico','Local do Serviço');
+			/*Deixa o campo data_solicitacao invisivel*/	 
+			$crud->field_type('data_solicitacao','invisible');
+			$crud->required_fields('descricao_equi','descricao_servico','patrimonio');
+						
+			$crud->set_subject('Solicitação - Sistemas');
+			
+			$crud->unset_back_to_list();
+			/*Insere a data de solicitação automaticamente via callback*/
+			$crud->callback_before_insert(array($this,'data_solicitacao_callback'));
+			$crud->set_field_upload('anexo','assets/arquivos/anexo/solicitacao_equi');
+			$output = $crud->render();
+			$this->template->load('home','templates/view_frm_solicitacao_equi',$output);
+
+		}catch(Exception $e){
+
+			show_error($e->getMessage().' --- '.$e->getTraceAsString());
+		}
 
 	}
 	/*
