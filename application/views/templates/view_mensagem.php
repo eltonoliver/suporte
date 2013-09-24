d8d039882324667d3a770d01fea72d2d4dcc1d74
Elton <eltonknoxville@hotmail.com>
2013-09-23 23:55:46 -0400

    Alteração da view
    Montando a view

--- application/views/templates/view_mensagem.php
+++ application/views/templates/view_mensagem.php
@@ -4,13 +4,34 @@
 	$idSolicitacao = $this->uri->segment(3);
 
 	$dados = $this->solicitacao_model->getSolicitacao($idSolicitacao);
-	echo '<pre>';
+	/*echo '<pre>';
 	foreach ($dados as $value) {
 
 		print_r($value);
-	}
+	}*/
 
 
  ?>
 
- <div> <h3> Solicitação N. <?php echo $dados[0]->id; ?>  </h3> </div>
+<?php if($dados[0]->tipo == 1){ ?>
+<div> <h3> Solicitação N. <?php echo $dados[0]->id; ?>  </h3> </div>
+<p><strong> Patrimônio: </strong>	<?php echo $dados[0]->patrimonio; ?>  </p>
+<p><strong> Descrição do Equipamento: </strong>	<?php echo $dados[0]->descricao_equi; ?>  </p>
+<p><strong> Local do Serviço: </strong>	<?php echo $dados[0]->uni_nomecompleto; ?>  </p>
+<p><strong> Data da Solicitação: </strong>	<?php echo $dados[0]->data_solicitacao; ?>  </p>
+<p><strong> Situação: </strong>	<?php echo $dados[0]->nomeSituacao; ?>  </p>
+
+
+
+
+
+
+<?php }else{ ?>
+
+	
+
+	sistemas
+
+
+
+<?php } ?>
