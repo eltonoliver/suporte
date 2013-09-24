ed34a06f7415b5d20d06239b583cd58ef657bc9e
Elton <eltonknoxville@hotmail.com>
2013-09-23 23:32:40 -0400

    Alteração de sql
    Alteração de sql

--- application/models/solicitacao_model.php
+++ application/models/solicitacao_model.php
@@ -5,14 +5,39 @@
  * and open the template in the editor.
  */
 
-class Solicitacao_model extends grocery_CRUD_Model{
+class Solicitacao_model extends CI_Model{
 
 
+	public function __construct(){
+	   parent::__construct();
+	}
+
+
+	   public function getSolicitacao($id = null){
 	
-
-
-
-
+	    	return $this->db->select(
+	    		'
+	    			SELECT solicitacao.id, 
+					solicitacao.descricao_equi, 
+					solicitacao.descricao_servico, 
+					usuarios.nome, 
+					db_base.unidade_uni.uni_nomecompleto, 
+					solicitacao.tipo, 
+					patrimonio.patrimonio, 
+					solicitacao.data_solicitacao, 
+					solicitacao.usuario_id, 
+					sistemas.nome, 
+					situacao.nome
+				FROM solicitacao LEFT JOIN usuarios ON solicitacao.id_suporte = usuarios.id
+					 LEFT JOIN db_base.unidade_uni ON solicitacao.local_servico = db_base.unidade_uni.id
+					 LEFT JOIN patrimonio ON solicitacao.patrimonio_id = patrimonio.id
+					 LEFT JOIN sistemas ON solicitacao.sistemas_id = sistemas.id
+					 LEFT JOIN situacao ON solicitacao.situacao_id = situacao.id
+				WHERE solicitacao.id = 34'
+	    		);
+	    }
+	
+	
 
 
 }
