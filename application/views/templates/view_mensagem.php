ec0263f20c3540b9f8464a0329bf75c3bc2172d1
Elton <eltonknoxville@hotmail.com>
2013-09-23 23:32:57 -0400

    Alteração na view
    Alteração na view

--- application/views/templates/view_mensagem.php
+++ application/views/templates/view_mensagem.php
@@ -1,1 +1,14 @@
-mensagem
+<?php 
+
+
+	$idSolicitacao = $this->uri->segment(3);
+
+	$dados = $this->solicitacao_model->getSolicitacao($idSolicitacao);
+	echo "<pre>";
+	print_r($dados);
+
+
+
+ ?>
+
+ <div> <h3> Solicitação N. <?php echo $dados[0]->id; ?>  </h3> </div>
