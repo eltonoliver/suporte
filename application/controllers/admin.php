42ef6528903baf5bc8080df1de34f11421f9fc62
Elton <eltonknoxville@hotmail.com>
2013-09-25 00:31:50 -0400

    Inserção de state
    Inserção de regras no estado

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -18,10 +18,29 @@ class Admin extends CI_Controller{
 			$crud = new grocery_CRUD();
 			$crud->set_theme('datatables');
 			$crud->set_table('solicitacao');
+				/*STATE*/
+			$state = $crud->getState();
+    		$state_info = $crud->getStateInfo();
+			if($state == 'read'){
+        		$idSolicitacao = $state_info->primary_key;
+    			$tipo = $this->solicitacao_model->getTipoSolicitacao($idSolicitacao);
+        		foreach ($tipo as $value) {
+        			if($value->tipo == 2){
+        				$crud->field_type('descricao_equi', 'hidden');
+        				$crud->field_type('patrimonio', 'hidden');
+        				$crud->field_type('patrimonio_id', 'hidden');
+
+ 
+        			}
+        		}
+    		}
+    		/*END STATE*/		 
+
+
 			$crud->set_relation('id_suporte','usuarios','nome');
 			$crud->set_relation('situacao_id','situacao','nome');	
 			$crud->set_relation('prioridade_id','prioridade','nome');	
-			$crud->set_relation('patrimonio_id','prioridade','nome');
+			$crud->set_relation('patrimonio_id','patrimonio','patrimonio');
 			$crud->set_relation('sistemas_id','sistemas','nome');
 			$crud->set_relation('local_servico','db_base.unidade_uni','uni_nomecompleto');	
 			$crud->unset_edit_fields('data_atualizacao','data_finalizacao','tipo');	
@@ -31,10 +50,21 @@ class Admin extends CI_Controller{
 			$crud->display_as('id','Código')->display_as('id_suporte','Nome do Suporte')
 				 ->display_as('situacao_id','Situação')
 				 ->display_as('data_solicitacao','Data de Solicitação')
-				 ->display_as('usuario_id','Nome do usuário');
+				 ->display_as('usuario_id','Nome do usuário')
+				 ->display_as('descricao_equi','Descrição do Equipamento')
+				 ->display_as('descricao_servico','Descrição do Serviço')
+				 ->display_as('local_servico','Local do Serviço')
+				 ->display_as('prioridade_id','Prioridade')
+				 ->display_as('sistemas_id','Nome do Sistema')
+				 ->display_as('patrimonio_id','Patrimônio');
 
-			$crud->unset_print();		 
+			$crud->unset_print();
+
+		
 			$output = $crud->render();
+
+ 
+
 
 			$this->template->load('home','templates/view_atendimento',$output);
 
