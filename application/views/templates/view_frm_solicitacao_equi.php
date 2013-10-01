0ec49e5cfe5a5d09674b2fd523d302cfef2543eb
Elton Oliveira <eltonknoxville@hotmail.com>
2013-09-30 23:58:45 -0300

    Criação de um alerta
    Criação de um alerta

--- application/views/templates/view_frm_solicitacao_equi.php
+++ application/views/templates/view_frm_solicitacao_equi.php
@@ -1,2 +1,15 @@
 
-<?php echo $output; ?>
+<?php 
+$verifica = $this->solicitacao_model->verificaSolicitacoes($this->sessionUsuario, 1 , 1);
+if($verifica < 1){
+	echo $output;
+
+}else{
+
+	echo '<div class="alert alert-block">
+			  <button type="button" class="close" data-dismiss="alert">×</button>
+			  <h4>Advertência!</h4>
+			<center> Você já efetuou um chamado relacionado a equipamentos! </center>
+		</div>'.br(15);
+}
+ ?>
