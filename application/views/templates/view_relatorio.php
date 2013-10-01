9cfa8b9e4e60d17e6eed67d970bae9b86855d31e
Elton Oliveira <eltonknoxville@hotmail.com>
2013-10-01 00:52:21 -0300

    alteracao na view relatorio
    alteracao na view relatorio

--- application/views/templates/view_relatorio.php
+++ application/views/templates/view_relatorio.php
@@ -1,1 +1,26 @@
-<?php echo $output; ?>
+
+<h4><center>Relatórios</center></h4>
+<br>
+Técnico : 
+
+<select name="tecnico" > 
+	<option value="1">Todos</option>
+	<option value="2">Elton Oliveira</option>
+	<option value="3">Paulo Santangelo</option>
+	<option value="4">Fernando Mauricio</option>
+
+</select>
+<br />
+Situação :
+
+<select name="situacao" > 
+	<option value="1">Todas</option>
+	<option value="2">Finalizado</option>
+	<option value="3">Enviado para as ..</option>
+
+</select>
+
+Data Início : 
+
+Data Fim :
+
