3c00272c7d907e2770eb6716e1ebeb1a26ea8066
Elton Oliveira <eltonknoxville@hotmail.com>
2013-09-30 23:59:24 -0300

    verificação usuário
    verifica se o usuário pode abrir um novo chamado

--- application/views/templates/view_frm_solicitacao_sis.php
+++ application/views/templates/view_frm_solicitacao_sis.php
@@ -1,1 +1,15 @@
-Formulário Solicitações
+
+<?php 
+$verifica = $this->solicitacao_model->verificaSolicitacoes($this->sessionUsuario, 2 , 1);
+if($verifica < 1){
+	echo $output;
+
+}else{
+
+	echo '<div class="alert alert-block">
+			  <button type="button" class="close" data-dismiss="alert">×</button>
+			  <h4>Advertência!</h4>
+			<center> Você já efetuou um chamado relacionado a sistemas! </center>
+		</div>'.br(15);
+}
+ ?>
