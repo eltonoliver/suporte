353d5a911c6647cd48093924a0c248588d29e280
Elton Oliveira <eltonknoxville@hotmail.com>
2013-10-03 11:30:42 -0400

    Inserção de verificação
    Inserção de verificação

--- application/views/templates/view_frm_solicitacao_sis.php
+++ application/views/templates/view_frm_solicitacao_sis.php
@@ -1,9 +1,14 @@
 <?php 
+$verifica = $this->solicitacao_model->verificaSolicitacoes($this->sessionUsuario, 2 , 1);
+ if($verifica < 4){
+	echo $output;
+ 
+}else{
 
 	echo '<div class="alert alert-block">
 			  <button type="button" class="close" data-dismiss="alert">×</button>
 			  <h4>Advertência!</h4>
 			<center> Você já efetuou um chamado relacionado a sistemas! </center>
 		</div>'.br(15);
-
+}
  ?>
