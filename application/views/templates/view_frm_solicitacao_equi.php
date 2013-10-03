128b961ef096fb2e80c00ccd3b90762b76f9abdd
Elton Oliveira <eltonknoxville@hotmail.com>
2013-10-03 11:30:27 -0400

    Inserção de verificação
    Inserção de verificação

--- application/views/templates/view_frm_solicitacao_equi.php
+++ application/views/templates/view_frm_solicitacao_equi.php
@@ -1,9 +1,15 @@
 <?php 
+$verifica = $this->solicitacao_model->verificaSolicitacoes($this->sessionUsuario, 1 , 1);
+ if($verifica < 4){
+	echo $output;
+ 
+}else{
+
 
 	echo '<div class="alert alert-block">
 			  <button type="button" class="close" data-dismiss="alert">×</button>
 			  <h4>Advertência!</h4>
 			<center> Você já efetuou um chamado relacionado a equipamentos! </center>
 		</div>'.br(15);
-
+}
  ?>
