5bd68abb294dcf2076a8644b471424961fa924f1
Elton Oliveira <eltonknoxville@hotmail.com>
2013-10-23 10:28:12 -0400

    Criação da área de feedback para o usuário
    Criação da área de feedback para o usuário

--- application/views/templates/view_atendimento.php
+++ application/views/templates/view_atendimento.php
@@ -3,6 +3,25 @@
 <?php
 
 echo $output;
+
+
 ?>
 
+<?php 
+/*****************************************
+*TELA DO CHAT DE RETORNO
+/*****************************************/		
+	$controller =  $this->uri->rsegment(2);
+	$action     =  $this->uri->rsegment(3);
 
+	if($controller == 'atendimentos' && $action == 'read'){
+?>
+
+	<center><h4> Chat Suporte </h4></center>
+
+
+
+
+
+
+<?php } ?>
